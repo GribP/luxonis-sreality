34e0c842f699c99a6d81d8f34bf658667b83c97d
grib.platon <grib.platon@outlook.com>
2023-06-06 16:17:48 +0200

    Use DB from extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
+from config import BaseConfig
+
+from app.extensions import db
+
 
 def create_app():
   app = Flask(__name__)
+  app.config.from_object(BaseConfig)
+
+  db.init_app(app)
 
   return app
