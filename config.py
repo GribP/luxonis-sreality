89f3126bffd50a6e8553c403257a8d9a93ee0c36
grib.platon <grib.platon@outlook.com>
2023-06-06 16:28:33 +0200

    Using dotenv file for config

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class BaseConfig:
   SECRET_KEY = os.environ.get('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', f'sqlite:///app.db')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', f'sqlite:///{ basedir }/app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
